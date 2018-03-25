5e504bc4bbc7bca8122afdb77a6507c65869bd5d
arman76 <armgham92@gmail.com>
2018-03-25 13:32:50 +0430

    Add files via upload

---
+++ app.py
@@ -0,0 +1,251 @@
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
+                          ConversationHandler)
+import scrp
+import _thread
+import time_table_file
+
+CHOOSING, TYPING_REPLY, TYPING_CHOICE, USERPASS, START_TIME, FINISH_TIME, COMMENTS, LESSON, PROFESSOR = range(9)
+
+reply_keyboard = [['فرستادن نام کاربری و کلمه عبور (username, password)'],
+                  ['گرفتن برنامه از erp'],
+                  ['ویرایش برنامه', 'گرفتن برنامه ویرایش شده']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def start(bot, update):
+    update.message.reply_text(
+        'سلام! کار من اینه که با استفاده از یوزر و پسورد erp وارد سامانه بشم'
+        ' و برنامه تحصیلی تو رو به شکل منظمی دربیارم و تحویلت بدم!!',
+        reply_markup=markup
+    )
+    return CHOOSING
+
+
+def user_pass(bot, update, user_data):
+    user_data['choice'] = 'username'
+    update.message.reply_text('خب {} خودتو بده:'.format('نام کاربری'))
+    return USERPASS
+
+
+def received_userpass(bot, update, user_data):
+    text = update.message.text
+    category = user_data['choice']
+    user_data[category] = text
+    if category == 'username':
+        update.message.reply_text('خب {} خودتو بده:'.format('کلمه عبور'))
+        user_data['choice'] = 'password'
+        return USERPASS
+    del user_data['choice']
+    if 'time_table' in user_data:
+        update.message.reply_text('خب الان برنامه رو از erp میگیرم!')
+        _thread.start_new_thread(scrp.main, (user_data, bot, update))
+        del user_data['time_table']
+        bot.send_message(chat_id=update.message.chat.id, text='یه ذره صبر کن!')
+        return CHOOSING
+    update.message.reply_text('خب اطلاعات گرفته شد! الان میتونی برنامه رو از erp بگیری!',
+                              reply_markup=markup)
+    return CHOOSING
+
+
+def time_table_scrp(bot, update, user_data):
+    if 'username' not in user_data:
+        user_data['time_table'] = 1
+        update.message.reply_text('اول باید نام کاربری و کلمه عبور رو بفرستی!')
+        bot.send_message(chat_id=update.message.chat_id, text='خب {} خودتو بده:'.format('نام کاربری'))
+        user_data['choice'] = 'username'
+        return USERPASS
+
+    _thread.start_new_thread(scrp.main, (user_data, bot, update))
+    bot.send_message(chat_id=update.message.chat.id, text='یه ذره صبر کن!')
+    return CHOOSING
+
+
+def time_table(bot, update, user_data):
+    _thread.start_new_thread(time_table_file.main, (user_data, bot, update))
+    return CHOOSING
+
+
+def edit(bot, update, user_data):
+    if 'exams' not in user_data:
+        update.message.reply_text('تو که هنوز برنامه ای نداری که بخوای ویرایشش کنی!! اول برنامتو بگیر بعد!',
+                                  reply_markup=markup)
+        return CHOOSING
+    edit_keyboard = [['ایجاد بخش جدید', 'حذف یک بخش']]
+    edit_markup = ReplyKeyboardMarkup(edit_keyboard, one_time_keyboard=True)
+    update.message.reply_text('یکی رو انتخاب کن (اگه وسط راه پشیمون شدی میتونی /cancel رو بفرستی):',
+                              reply_markup=edit_markup)
+    return CHOOSING
+
+
+def day(bot, update, user_data):
+    text = update.message.text
+    if text == 'ایجاد بخش جدید':
+        user_data['edit_mode'] = 'create'
+    elif text == 'حذف یک بخش':
+        user_data['edit_mode'] = 'remove'
+    days_keyboard = [['شنبه', 'يکشنبه', 'دوشنبه'],
+                     ['سه شنبه', 'چهارشنبه', 'پنجشنبه']]
+    days_markup = ReplyKeyboardMarkup(days_keyboard, one_time_keyboard=True)
+    update.message.reply_text('چه روزی؟ :', reply_markup=days_markup)
+    return CHOOSING
+
+
+def start_time(bot, update, user_data):
+    text = update.message.text
+    user_data['edit'] = []
+    user_data['edit'].append(text)
+    update.message.reply_text('ساعت شروع؟ (مثلا 8:00) :')
+    return START_TIME
+
+
+def received_start_time(bot, update, user_data):
+    text = update.message.text
+    user_data['edit'].append(text)
+    if user_data['edit_mode'] == 'remove':
+        '''
+        '''
+        bot.send_message(chat_id=update.message.chat.id, text='remmm', reply_markup=markup)
+        return CHOOSING
+    # if text return get_table
+    update.message.reply_text('ساعت پایان؟ (مثلا 13:30) :')
+    return FINISH_TIME
+
+
+def received_finish_time(bot, update, user_data):
+    text = update.message.text
+    user_data['edit'].append(text)
+    update.message.reply_text('توضیحات؟ (مثلا کلاس کجا برگزار میشه مثل ک۳-فنی یا هر چیز دیگه ای) :')
+    return COMMENTS
+
+
+def received_comments(bot, update, user_data):
+    text = update.message.text
+    user_data['edit'].append(text)
+    update.message.reply_text('درس؟ (مثلا حل تمرین ریاضی) :')
+    return LESSON
+
+
+def received_lesson(bot, update, user_data):
+    text = update.message.text
+    user_data['edit'].append(text)
+    update.message.reply_text('اسم استاد یا ارائه دهنده :')
+    return PROFESSOR
+
+
+def received_professor(bot, update, user_data):
+    text = update.message.text
+    user_data['edit'].append(text)
+    user_data['info'].append('\t'.join(user_data['edit']))
+    del user_data['edit']
+    _thread.start_new_thread(time_table_file.main, (user_data, bot, update))
+    return CHOOSING
+
+
+def cancel_edit(bot, update, user_data):
+    del user_data['edit']
+    update.message.reply_text('کنسل شد!', reply_markup=markup)
+    return CHOOSING
+
+
+def received_information(bot, update, user_data):
+    text = update.message.text
+    print(text)
+    category = user_data['choice']
+    user_data[category] = text
+    del user_data['choice']
+
+    update.message.reply_text('sss',
+                              reply_markup=markup)
+    return CHOOSING
+
+
+def main():
+    updater = Updater('517255695:AAFSQ549HEYNGhDCT3iC2dLgst1w5YPLOOA')
+
+    dp = updater.dispatcher
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            CHOOSING: [RegexHandler('^.*\(username\, password\)$',
+                                    user_pass,
+                                    pass_user_data=True),
+                       RegexHandler('^گرفتن برنامه از erp$',
+                                    time_table_scrp,
+                                    pass_user_data=True),
+                       RegexHandler('^ویرایش برنامه$',
+                                    edit,
+                                    pass_user_data=True),
+                       RegexHandler('^(ایجاد بخش جدید|حذف یک بخش)$',
+                                    day,
+                                    pass_user_data=True),
+                       RegexHandler('^گرفتن برنامه ویرایش شده$',
+                                    time_table,
+                                    pass_user_data=True),
+                       RegexHandler('^(پنجشنبه|سه شنبه|دوشنبه|چهارشنبه|يکشنبه|شنبه)$',
+                                    start_time,
+                                    pass_user_data=True),
+                       CommandHandler('cancel',
+                                      cancel_edit,
+                                      pass_user_data=True)
+                       ],
+
+            USERPASS: [MessageHandler(Filters.text,
+                                      received_userpass,
+                                      pass_user_data=True)],
+
+            START_TIME: [MessageHandler(Filters.text,
+                                        received_start_time,
+                                        pass_user_data=True),
+                         CommandHandler('cancel',
+                                        cancel_edit,
+                                        pass_user_data=True)],
+
+            FINISH_TIME: [MessageHandler(Filters.text,
+                                         received_finish_time,
+                                         pass_user_data=True),
+                          CommandHandler('cancel',
+                                         cancel_edit,
+                                         pass_user_data=True)],
+
+            COMMENTS: [MessageHandler(Filters.text,
+                                      received_comments,
+                                      pass_user_data=True),
+                       CommandHandler('cancel',
+                                      cancel_edit,
+                                      pass_user_data=True)],
+
+            LESSON: [MessageHandler(Filters.text,
+                                    received_lesson,
+                                    pass_user_data=True),
+                     CommandHandler('cancel',
+                                    cancel_edit,
+                                    pass_user_data=True)],
+
+            PROFESSOR: [MessageHandler(Filters.text,
+                                       received_professor,
+                                       pass_user_data=True),
+                        CommandHandler('cancel',
+                                       cancel_edit,
+                                       pass_user_data=True)],
+
+            TYPING_REPLY: [MessageHandler(Filters.text,
+                                          received_information,
+                                          pass_user_data=True)],
+        },
+
+        fallbacks=[]
+    )
+
+    dp.add_handler(conv_handler)
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+
+# '517255695:AAFSQ549HEYNGhDCT3iC2dLgst1w5YPLOOA'
---
+++ time_table_file.py
@@ -0,0 +1,153 @@
+import os
+import re
+import telegram
+import matplotlib.pyplot as plt
+from bidi.algorithm import get_display
+import persian_reshaper
+import matplotlib.font_manager as fm
+from telegram import ReplyKeyboardMarkup
+
+
+reply_keyboard = [['فرستادن نام کاربری و کلمه عبور (username, password)'],
+                  ['گرفتن برنامه از erp'],
+                  ['ویرایش برنامه', 'گرفتن برنامه ویرایش شده']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def ds(day):
+    if day == get_display(persian_reshaper.reshape('شنبه')):
+        return 0
+    elif day == get_display(persian_reshaper.reshape('يکشنبه')):
+        return 1
+    elif day == get_display(persian_reshaper.reshape('دوشنبه')):
+        return 2
+    elif day == get_display(persian_reshaper.reshape('سه شنبه')):
+        return 3
+    elif day == get_display(persian_reshaper.reshape('چهارشنبه')):
+        return 4
+    elif day == get_display(persian_reshaper.reshape('پنجشنبه')):
+        return 5
+    return -1
+
+
+def main(user_data, bot, update):
+    days = []
+    starts = []
+    ends = []
+    coms = []
+    darss = []
+
+    colors = ['pink', 'lightgreen', 'lightblue', 'crimson', 'salmon', 'yellow', 'silver', 'yellowgreen', 'y', 'tan',
+              'orchid', 'c', 'aqua', 'deeppink']
+
+    for line in user_data['info']:
+        data = line.split('\t')
+        days.append(get_display(persian_reshaper.reshape(data[0])))
+        starts.append(float(data[1].split(':')[0]) + float(data[1].split(':')[1]) / 60)
+        ends.append(float(data[2].split(':')[0]) + float(data[2].split(':')[1]) / 60)
+        if len(data) == 6:
+            coms.append(get_display(persian_reshaper.reshape(data[3])))
+        darss.append(get_display(persian_reshaper.reshape(data[4])))
+    days2 = set(days)
+    prop = fm.FontProperties(fname='XP ZibaBd.ttf')
+    dd = dict((ds(x), x) for x in days2)
+    sorted_days = []
+    for key in sorted(dd):
+        sorted_days.append(dd[key])
+
+    darss = list(set(darss))
+    nl = dict((darss[x], x) for x in range(len(darss)))
+    mn = min(starts)
+    mx = max(ends)
+    fig = plt.figure(figsize=(16, 9))
+    for i, line in zip(range(len(days)), user_data['info']):
+        data = line.split('\t')
+        temp3 = []
+        if len(data[3]) > 30:
+            for iji in range(int(len(data[3]) / 32) + 1):
+                temp3.append(data[3][iji * 32:32 * (iji + 1)])
+            data[3] = '\n'.join(temp3)
+        temp5 = []
+        if len(data[5]) > 32:
+            for ijl in range(int(len(data[5]) / 34) + 1):
+                temp5.append(data[5][ijl * 34:34 * (ijl + 1)])
+            data[5] = '\n'.join(temp5)
+
+        plt.fill_between([starts[i] + 0.05, ends[i] - 0.05],
+                         [sorted_days.index(days[i]) + 0.53, sorted_days.index(days[i]) + 0.53],
+                         [sorted_days.index(days[i]) + 1.3, sorted_days.index(days[i]) + 1.43],
+                         color=colors[nl[get_display(persian_reshaper.reshape(data[4]))]], edgecolor='red', linewidth=5)
+
+        temp4 = []
+        if len(data[4]) > 24:
+            for ijk in range(int(len(data[4]) / 27) + 1):
+                temp4.append(data[4][ijk * 27:27 * (ijk + 1)])
+            data[4] = '\n'.join(temp4)
+
+        plt.text(starts[i] + 0.25, sorted_days.index(days[i]) + 0.55,
+                 '{0}:{1:0>2}'.format(int(data[1].split(':')[0]), int(data[1].split(':')[1])), va='top', fontsize=7)
+        plt.text(ends[i] - 0.05, sorted_days.index(days[i]) + 0.55,
+                 '{0}:{1:0>2}'.format(int(data[2].split(':')[0]), int(data[2].split(':')[1])), va='top', fontsize=7)
+        plt.text((starts[i] + ends[i]) * 0.5,
+                 (sorted_days.index(days[i]) + 0.5 + sorted_days.index(days[i]) + 1.4) * 0.5 - 0.2,
+                 get_display(persian_reshaper.reshape(data[4])), ha='center', va='center', fontproperties=prop,
+                 fontsize=13 - 1.5 * len(temp4))
+        plt.text((starts[i] + ends[i]) * 0.5,
+                 (sorted_days.index(days[i]) + sorted_days.index(days[i]) + 2.05) * 0.5,
+                 get_display(persian_reshaper.reshape(data[3])), ha='center', va='center', fontsize=9 - len(temp3))
+        plt.text((starts[i] + ends[i]) * 0.5,
+                 (sorted_days.index(days[i]) + sorted_days.index(days[i]) + 2) * 0.5 + 0.23,
+                 get_display(persian_reshaper.reshape(data[5])), ha='center', va='center', fontsize=8 - len(temp5))
+    examsb = []
+    for line in user_data['exams']:
+        pattern = r'^.*(?P<y>\d{4})\/(?P<m>\d{2})\/(?P<d>\d{2}).*از ((?P<h>\d{2})\:\d{2}) تا.*$'
+        examsb.append(re.search(pattern, line))
+
+    exams = dict(
+        (int(k.group('h')) / 24 + int(k.group('d')) + (int(k.group('m')) - 1) * 31 + (int(k.group('y')) - 1) * 31 * 12,
+         list()) for k in examsb
+        if k is not None)
+
+    [exams[int(k.group('h')) / 24 + int(k.group('d')) + (int(k.group('m')) - 1) * 31 + (
+                int(k.group('y')) - 1) * 31 * 12].append(k.group()) for k
+     in examsb if k is not None]
+
+    jj = 0
+    exx = mx
+    for key in sorted(exams):
+        for j in exams[key]:
+            if jj == 8:
+                exx -= 4
+                jj = 0
+            plt.text(exx, (jj + 1.5 + 5.5 * len(sorted_days)) * 0.2, get_display(persian_reshaper.reshape(j)),
+                     fontsize=11)
+            jj += 1
+
+    ax = fig.add_subplot(111)
+
+    ax.set_ylim(len(days2) + 2 / 4, 0.4)
+    ax.set_xlim(mx, mn)
+    ax.set_yticks(range(1, len(days2) + 1))
+    ax.set_yticklabels(sorted_days)
+    ax.xaxis.grid()
+    ax2 = ax.twiny().twinx()
+    ax2.set_ylim(ax.get_ylim())
+    ax2.set_xlim(ax.get_xlim())
+    ax2.set_yticks(ax.get_yticks())
+    ax2.set_yticklabels(sorted_days)
+    plt.subplots_adjust(left=0.06, bottom=0.26, right=0.95, top=0.96, wspace=0.2, hspace=0.2)
+
+    bot.send_message(chat_id=update.message.chat.id, text='داره میاد!!')
+    bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.UPLOAD_DOCUMENT)
+    plt.savefig('{0}.pdf'.format(user_data['username'] + 'barn'))
+    plt.savefig('{0}.png'.format(user_data['username'] + 'barn'), dpi=150)
+    bot.send_document(chat_id=update.message.chat.id, document=open('{0}.png'.format(user_data['username'] + 'barn'),
+                                                                    'rb'))
+    bot.send_document(chat_id=update.message.chat.id, document=open('{0}.pdf'.format(user_data['username'] + 'barn'),
+                                                                    'rb'), reply_markup=markup)
+
+    # plt.show()
+    os.remove('{0}.png'.format(user_data['username'] + 'barn'))
+    os.remove('{0}.pdf'.format(user_data['username'] + 'barn'))
+    plt.close()
+    # bot.send_message(chat_id=update.message.chat.id, text='میتونی حتی به برنامت اضافه هم بکنی:\n''/j')
