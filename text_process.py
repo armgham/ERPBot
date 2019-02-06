73b5ef7d545fb18252870eb18dba8bc470f9f8b7
arman76 <armgham92@gmail.com>
2019-02-06 18:07:05 +0330

    Update text_process.py

--- text_process.py
+++ text_process.py
@@ -11,69 +11,25 @@ markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
 def main(user_data, bot, update):
     user_data['scrp_info'] = []
-    for line in user_data['first_info']:
-        line = line.replace('**', '')
-        ostad = re.search(r'\t{2}\(\(\((?P<ostad>.*$)', line).group('ostad')
-        dars = re.search(r'\t{3}(?P<dars>.*?)\t', line).group('dars')
-        line = line[0:line.find('\t\t\t')]
-        p = r'.*?(?=چهارشنبه|سه شنبه|پنج شنبه|دوشنبه|يکشنبه|شنبه)'
-        p2 = r'(?P<day>چهارشنبه|سه شنبه|دوشنبه|پنج شنبه|يکشنبه|شنبه)\s+(?P<start>\d{2}\:\d{2})\s+\-\s+(?P<end>\d{2}\:\d{2})\s*'
-        pb = r'(چهارشنبه|سه شنبه|پنج شنبه|دوشنبه|يکشنبه|شنبه)'
-        while len(line) != 0:
+    for lines in user_data['first_info']:
+        ostad = re.search(r'\t{2}\(\(\((?P<ostad>.*$)', lines).group('ostad')
+        dars = re.search(r'\t{3}(?P<dars>.*?)\t', lines).group('dars')
+        lines = line[0:lines.find('\t\t\t')]
+        lines = list(filter(lambda x: x != '', lines.split('**')))
+        p = r'(?P<first_comment>.*?)\s*(?P<day>چهارشنبه|سه شنبه|دوشنبه|يکشنبه|شنبه)\s*(?P<second_comment>.*?)\s*(?P<start>\d{2}\:\d{2})\s+\-\s+(?P<end>\d{2}\:\d{2})\s*(?P<last_comment>.*)'
+        for line in lines:
             try:
-                tozih = []
-
-                xxx = [m.end(0) for m in re.finditer(p, line)][0]
-                tozih.append(line[0:xxx])
-
-                if len([m.start(0) for m in re.finditer(p2, line)]) == 0:
-                    raise ValueError
-
-                if [m.start(0) for m in re.finditer(pb, line)][0] != [m.start(0) for m in re.finditer(p2, line)][0]:
-                    raise ValueError
-
-                line = line[[m.end(0) for m in re.finditer(p, line)][0]:]
-                total_time = line[0:[m.end(0) for m in re.finditer(p2, line)][0]]
-
-                line = line[[(m.start(0), m.end(0)) for m in re.finditer(p2, line)][0][1]:]
-                while not (re.sub('[()]', '', line.split(' ')[0]) in ostad or line.split(' ')[0] in '(هفته زوج)' or
-                           line.split(' ')[0] in '(هفته فرد)') and len(line) != 0:
-                    if len([m.start(0) for m in re.finditer(p2, line)]) != 0:
-                        if [m.start(0) for m in re.finditer(p2, line)][0] != 0:
-                            tozih.append(line.split(' ')[0])
-                            line = line[line.find(' ') + 1:]
-                        else:
-                            break
-                    elif len(line) != 0:
-                        tozih.append(line)
-                        line = ''
-                zaman = re.search(p2, total_time)
-                if zaman is not None:
-                  user_data['scrp_info'].append(zaman.group('day') + '\t' + zaman.group('start') + '\t' + zaman.group('end') + '\t' + ' '.join(tozih) + '\t' + dars + '\t' + ostad)
+                res = re.search(p, line)
+                if res.group('day') == '' or res.group('start') == '' or res.group('end') == '':
+                  raise ValueError
+                user_data['scrp_info'].append(
+                    res.group('day') + '\t' + res.group('start') + '\t' + res.group('end') + '\t' + ' '.join([res.group('first_comment'), res.group('second_comment'), res.group('last_comment')]) + '\t' + dars + '\t' + ostad)
 
             except Exception as e:
                 print(dars)
                 print(e.args)
                 print(user_data)
-                dell = [line[0:[m.end(0) for m in re.finditer(pb, line)][0]]]
-                line = line[[m.end(0) for m in re.finditer(pb, line)][0]:]
-                while line[0] in ' \t':
-                    line = line[1:]
-
-                while not (re.sub('[()]', '', line.split(' ')[0]) in ostad or line.split(' ')[0] in '(هفته زوج)' or
-                           line.split(' ')[0] in '(هفته فرد)') and len(line) != 0:
-                    if len([m.start(0) for m in re.finditer(p2, line)]) != 0:
-                        if [m.start(0) for m in re.finditer(p2, line)][0] != 0:
-                            dell.append(line.split(' ')[0])
-                            line = line[line.find(' ') + 1:]
-                        else:
-                            break
-                    elif len(line) != 0:
-                        dell.append(line)
-                        line = ''
-
-                bot.send_message(chat_id=update.message.chat.id, text='درس ' + dars + ' : \"' + ' '.join(
-                    dell) + ' ' + ostad + ' \"' + 'یه مشکلی داره نتونستم بیارم تو برنامه. اگه خواستی مبتونی با '
+                bot.send_message(chat_id=update.message.chat.id, text='درس ' + dars + ' : \"' + line + ' ' + ostad + ' \"' + 'یه مشکلی داره نتونستم بیارم تو برنامه. اگه خواستی مبتونی با '
                                                   'استفاده از (ویرایش برنامه) دستی یه بخش جدید به برنامه اضافه کنی',
                                  reply_markup=markup)
                 continue
