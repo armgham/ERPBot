ce30211c27c003964887a5fb2351673468f1fbac
arman76 <armgham92@gmail.com>
2019-09-03 03:12:13 +0430

    Update scrp.py

--- scrp.py
+++ scrp.py
@@ -20,8 +20,8 @@ markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 def main(user_data, bot, update):
     driver = webdriver.PhantomJS()
     try:
-        driver.get("http://erp.guilan.ac.ir/Dashboard.aspx")
-        if 'erp.guilan.ac.ir/GoToDashboard.aspx' in driver.current_url:
+        driver.get("http://sada.guilan.ac.ir/Dashboard.aspx")
+        if 'sada.guilan.ac.ir/GoToDashboard.aspx' in driver.current_url:
             driver.find_element_by_class_name('refreshDash').click()
         
         
